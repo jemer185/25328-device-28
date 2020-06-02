cd579169dc750bb3137b4c2bf9289c164842d6a4
Keks <keks@htmlacademy.ru>
2020-06-02 12:15:29 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/script.js
@@ -0,0 +1,19 @@
+
+var servicesControls = document.querySelector(".services__controls");
+var servicesLinks = document.querySelectorAll(".services__switch");
+var servicesItems = document.querySelectorAll(".services__item");
+
+servicesControls.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  var target = evt.target;
+  console.log(target);
+  if(target.classList.contains("services__switch")) {
+    for(var i = 0; i < servicesItems.length; i++) {
+      servicesItems[i].classList.remove("services__item-show");
+      servicesLinks[i].classList.remove("services__switch-active");
+    }
+    target.classList.add("services__switch-active");
+    var currentItem = document.querySelector(".services__item-" + target.id);
+    currentItem.classList.add("services__item-show");
+  }
+});

14a261ac269c2836d89e79480c5b0b2e77cc5c35
Keks <keks@htmlacademy.ru>
2020-06-08 17:00:12 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/script.js
+++ 4/js/script.js
@@ -1,26 +1,71 @@
-var servicesControls = document.querySelector(".services__controls");
-var servicesLinks = document.querySelectorAll(".services__switch");
-var servicesItems = document.querySelectorAll(".services__item");
+'use strict';
+// Кнопка выпадающего меню //
 
-servicesControls.addEventListener("click", function (evt) {
+var showNavigationLink = document.querySelector('.catalog-button');
+var showNavigation = document.querySelector('.site-nav__sublist');
+
+showNavigationLink.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  showNavigation.classList.toggle('sublist-visible');
+});
+
+// Сервис слайдер //
+
+var servicesControls = document.querySelector('.services__controls');
+var servicesLinks = document.querySelectorAll('.services__switch');
+var servicesItems = document.querySelectorAll('.services__item');
+
+servicesControls.addEventListener('click', function (evt) {
   evt.preventDefault();
   var target = evt.target;
-  console.log(target);
-  if(target.classList.contains("services__switch")) {
-    for(var i = 0; i < servicesItems.length; i++) {
-      servicesItems[i].classList.remove("services__item-show");
-      servicesLinks[i].classList.remove("services__switch-active");
+  if (target.classList.contains('services__switch')) {
+    for (var i = 0; i < servicesItems.length; i++) {
+      servicesItems[i].classList.remove('services__item-show');
+      servicesLinks[i].classList.remove('services__switch-active');
     }
-    target.classList.add("services__switch-active");
-    var currentItem = document.querySelector(".services__item-" + target.id);
-    currentItem.classList.add("services__item-show");
+    target.classList.add('services__switch-active');
+    var currentItem = document.querySelector('.services__item-' + target.id);
+    currentItem.classList.add('services__item-show');
   }
 });
 
-var showNavigationLink = document.querySelector(".catalog-button"),
-showNavigation = document.querySelector(".site-nav__sublist");
+// Модальное окно обратной связи и карта //
 
-showNavigationLink.addEventListener("click", function (evt) {
+var linkForm = document.querySelector('.feedback-button');
+var linkMap = document.querySelector('.map');
+var modalForm = document.querySelector('.modal__feedback');
+var modalMap = document.querySelector('.modal__map');
+var formClose = modalForm.querySelector('.modal__close');
+var mapClose = modalMap.querySelector('.modal__close');
+
+linkForm.addEventListener('click', function (evt) {
   evt.preventDefault();
-  showNavigation.classList.toggle("sublist-visible");
+  modalForm.classList.add('modal-show');
+});
+
+formClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalForm.classList.remove('modal-show');
+});
+
+linkMap.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalMap.classList.add('modal-show');
+});
+
+mapClose.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalMap.classList.remove('modal-show');
+});
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modalForm.classList.contains('modal-show')) {
+      modalForm.classList.remove('modal-show');
+    }
+    if (modalMap.classList.contains('modal-show')) {
+      modalMap.classList.remove('modal-show');
+    }
+  }
 });
